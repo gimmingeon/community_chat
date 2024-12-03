10cc0731dfffa0118fd53ffd36d9d8a8a7cc9279
gimmingeon <mnbqwe123987@naver.com>
2024-12-03 17:16:57 +0900

    기초 세팅

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("hellow da");
+});
+
+app.listen(port, () => {
+    console.log(`${port}번으로 서버가 열렸습니다.`);
+});
